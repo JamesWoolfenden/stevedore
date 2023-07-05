ca3428a4b98feeb14b7c353b24e17175c9241977
James Woolfenden <james.woolfenden@gmail.com>
2023-07-05 16:14:30 +0100

    WIP

--- go.mod
+++ go.mod
@@ -4,12 +4,16 @@ go 1.20
 
 require (
 	github.com/containerd/typeurl v1.0.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/moby/buildkit v0.11.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/zerolog v1.29.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.25.7 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,77 @@
 package main
 
 import (
+	"fmt"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	cli "github.com/urfave/cli/v2"
 	"os"
+	"sort"
 	stevedore "stevedore/src"
+	"stevedore/src/version"
+	"time"
 )
 
 func main() {
-	err := stevedore.Parse()
-	if err != nil {
-		os.Exit(1)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	var file string
+
+	var directory string
+
+	app := &cli.App{
+		EnableBashCompletion: true,
+		Flags:                []cli.Flag{},
+		Commands: []*cli.Command{
+			{
+				Name:      "version",
+				Aliases:   []string{"v"},
+				Usage:     "Outputs the application version",
+				UsageText: "stevedore version",
+				Action: func(*cli.Context) error {
+					fmt.Println(version.Version)
+
+					return nil
+				},
+			},
+			{
+				Name:      "label",
+				Aliases:   []string{"l"},
+				Usage:     "Updates Dockerfiles labels",
+				UsageText: "stevedore label",
+				Action: func(*cli.Context) error {
+					err := stevedore.Parse()
+					return err
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "file",
+						Aliases:     []string{"f"},
+						Usage:       "Dockerfile to parse",
+						Destination: &file,
+						Category:    "files",
+					},
+					&cli.StringFlag{
+						Name:        "directory",
+						Aliases:     []string{"d"},
+						Usage:       "Destination to update Dockerfiles",
+						Value:       ".",
+						Destination: &directory,
+						Category:    "files",
+					},
+				},
+			},
+		},
+		Name:     "stevedore",
+		Usage:    "Update Dockerfile labels",
+		Compiled: time.Time{},
+		Authors:  []*cli.Author{{Name: "James Woolfenden", Email: "jim.wolf@duck.com"}},
+		Version:  version.Version,
+	}
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal().Err(err).Msg("stevedore failure")
 	}
 }
