ab52f198f88804621d9285e28de7dc1b01ad9182
James Woolfenden <james.woolfenden@gmail.com>
2023-07-12 16:18:34 +0100

    tests

--- src/label_test.go
+++ src/label_test.go
@@ -1,6 +1,8 @@
 package stevedore_test
 
 import (
+	"os/user"
+	"reflect"
 	stevedore "stevedore/src"
 	"testing"
 
@@ -12,6 +14,7 @@ func TestLabel(t *testing.T) {
 
 	type args struct {
 		result *parser.Result
+		file   *string
 	}
 
 	tests := []struct {
@@ -26,8 +29,32 @@ func TestLabel(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			if got := stevedore.Label(tt.args.result); got != tt.want {
+			if got := stevedore.Label(tt.args.result, tt.args.file); got != tt.want {
 				t.Errorf("Label() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMakeLabel(t *testing.T) {
+	type args struct {
+		child   *parser.Node
+		layer   int64
+		myUser  *user.User
+		endLine int
+		file    *string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *parser.Node
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := stevedore.MakeLabel(tt.args.child, tt.args.layer, tt.args.myUser, tt.args.endLine, tt.args.file); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MakeLabel() = %v, want %v", got, tt.want)
 			}
 		})
 	}
