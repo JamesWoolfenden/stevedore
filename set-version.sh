94e0b77893aebb07848bffa0cf2cabc2d3c633de
James Woolfenden <james.woolfenden@gmail.com>
2023-07-13 16:59:50 +0100

    update to goreleaser script

--- set-version.sh
+++ set-version.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
 latestTag=$(git describe --tags)
 echo "Updating version file with new tag: $latestTag"
 echo "package version" > src/version/version.go
 echo "" >> src/version/version.go
+echo "// Version controls the applications version." >> src/version/version.go
 echo "const Version = \"$latestTag\"" >> src/version/version.go
